cdcd426c474376502eff0d6b2afc730372fd318f
willgibbons <85369637+willgibbons@users.noreply.github.com>
2023-02-05 16:02:12 -0500

    Add files via upload

---
+++ CSharpListObject/CSharpListObject/Form.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace CSharpListObject
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtString = new System.Windows.Forms.TextBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdd.Location = new System.Drawing.Point(329, 38);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(93, 121);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add string to List";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(193, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "string:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(178, 147);
+            this.listBox1.TabIndex = 2;
+            // 
+            // txtString
+            // 
+            this.txtString.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtString.Location = new System.Drawing.Point(234, 12);
+            this.txtString.Name = "txtString";
+            this.txtString.Size = new System.Drawing.Size(188, 20);
+            this.txtString.TabIndex = 3;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.btnRemove.Location = new System.Drawing.Point(234, 38);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(89, 121);
+            this.btnRemove.TabIndex = 4;
+            this.btnRemove.Text = "Remove string from List";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 167);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.txtString);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form";
+            this.Text = "Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtString;
+        private System.Windows.Forms.Button btnRemove;
+    }
+}
+
---
+++ CSharpListObject/CSharpListObject/Form.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace CSharpListObject
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        //Members
+        List<string> _listofstrings = null; // this is the standard list type // unused
+        BindingList<string> _ListOfStrings; // binding list allows for linking lists to diff objects
+
+        //Constructor
+        public Form()
+        {
+            InitializeComponent();
+
+            _ListOfStrings = new BindingList<string>(); // init Blist
+            listBox1.DataSource = _ListOfStrings;       // bind list to listbox
+
+            // add items to the list
+            _ListOfStrings.Add("Item 1: string");
+            _ListOfStrings.Add("Item 2: String");
+            _ListOfStrings.Add("Item 3: STRING");
+            // notice how they appear in the listbox on app launch
+        }
+
+        //Methods
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            _ListOfStrings.Add(txtString.Text);
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _ListOfStrings.RemoveAt(listBox1.SelectedIndex);
+            }
+            catch (ArgumentOutOfRangeException E)
+            {
+                // if there are no items in the list an OutOfRange exception occurs
+                // bandaid it by adding exception message to list
+                _ListOfStrings.Add(E.Message);
+            }
+        }
+
+    }//class
+}//namespace
